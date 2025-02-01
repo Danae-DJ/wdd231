c863add9c809735723fd4795b41e51d106637ed0
Danae-DJ <danae_taiyaki@hotmail.com>
2025-02-01 14:16:23 -0600

    added join html info

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,3 +1,27 @@
+// Set current year in footer
+const yearSpan = document.getElementById('currentYear');
+yearSpan.textContent = new Date().getFullYear();
+
+// Set last modified date
+const lastModified = document.getElementById('lastModified');
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
+
+// Toggle navigation menu
+const menuToggle = document.getElementById('menuToggle');
+const navLinks = document.getElementById('navLinks');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active'); // Toggle the 'active' class on the nav menu
+});
+
+
+
+
+
+
+
+
+
 // Grab the query string from the URL (everything after ?)
 const params = new URLSearchParams(window.location.search);
 

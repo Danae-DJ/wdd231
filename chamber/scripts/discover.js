e5609dd24d9c8adebc796984078eb56ab95d9128
Danae-DJ <danae_taiyaki@hotmail.com>
2025-02-08 00:08:05 -0600

    added desing and img

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -13,3 +13,27 @@ const navLinks = document.getElementById('navLinks');
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active'); // Toggle the 'active' class on the nav menu
 });
+
+
+//Visit message introduction
+document.addEventListener("DOMContentLoaded", () => {
+    const visitMessage = document.getElementById("visitMessage");
+    const lastVisit = localStorage.getItem("lastVisit");
+    const currentVisit = Date.now();
+
+    if (!lastVisit) {
+        visitMessage.textContent = "Welcome! Let us know if you have any questions.";
+    } else {
+        const daysPassed = Math.floor((currentVisit - lastVisit) / (1000 * 60 * 60 * 24));
+
+        if (daysPassed < 1) {
+            visitMessage.textContent = "Back so soon! Awesome!";
+        } else if (daysPassed === 1) {
+            visitMessage.textContent = `You last visited 1 day ago.`;
+        } else {
+            visitMessage.textContent = `You last visited ${daysPassed} days ago.`;
+        }
+    }
+
+    localStorage.setItem("lastVisit", currentVisit);
+});
